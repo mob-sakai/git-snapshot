53c5ec0e85051ee31df6d16a144df62899637849
mob-sakai <sakai861104@gmail.com>
2019-10-16 19:32:05 +0900

    feat: Support dry-run mode

--- index.js
+++ index.js
@@ -22,34 +22,42 @@ module.exports = function gitSnapshot(argv) {
     Promise.resolve()
       // Check permissions for remote branch.
       .then(async () => {
-        if (argv.remote) {
-          await git(['push', '--dry-run', '-f', argv.remote, argv.branch], onCwdOpts);
-          await git(['fetch', argv.remote], onCwdOpts);
-        }
+        if (!argv.remote) return;
+
+        debug(`Check permissions for remote branch: ${argv.remote} ${argv.branch}`);
+        await git(['push', '--dry-run', '-f', argv.remote, argv.branch], onCwdOpts);
+        await git(['fetch', argv.remote], onCwdOpts);
       })
 
       // Check that the tag does not exist.
       .then(async () => {
-        if (argv.tag && !argv.force) {
-          await git(['show-ref', '--verify', `refs/tags/${argv.tag}`], onCwdOpts)
-            .catch(() => false)
-            .then(exists => {
-              if (exists) {
-                const message = `tag '${argv.tag}' already exists`
-                throw { all: message, message,  };
-              }
-            });
-        }
+        if (!argv.tag || argv.force) return;
+
+        debug(`Check that the tag does not exist: ${argv.tag}`);
+        await git(['show-ref', '--verify', `refs/tags/${argv.tag}`], onCwdOpts)
+          .catch(() => false)
+          .then(exists => {
+            if (exists) {
+              const message = `tag '${argv.tag}' already exists`;
+              throw { all: message, message };
+            }
+          });
       })
 
       // Add worktree for the task.
       .then(async () => {
+        if (argv.dryRun) return;
+
+        debug(`Check that the tag does not exist: ${argv.tag}`);
         await git(['worktree', 'add', '--detach', worktreePath], onCwdOpts);
         isAddedWorktree = true;
       })
 
       // Checkout working branch.
       .then(async () => {
+        if (argv.dryRun) return;
+
+        debug(`Checkout working branch: ${onWorktreeOpts.cwd}`);
         await git(['checkout', '-B', argv.branch, refs], onWorktreeOpts) //
           .catch(async err => {
             await git(['checkout', '-B', argv.branch, `refs/heads/${argv.branch}`], onWorktreeOpts) //
@@ -61,36 +69,44 @@ module.exports = function gitSnapshot(argv) {
 
       // Remove/copy all files in directory.
       .then(async () => {
+        if (argv.dryRun) return;
+
+        debug(`Remove/copy all files in directory:`);
         await git(['rm', '-rf', '--ignore-unmatch', '.'], onWorktreeOpts);
         await fs.copy(path.join(cwd, argv.prefix), worktreePath);
       })
 
       // Commit files.
       .then(async () => {
+        if (argv.dryRun) return;
+
+        debug(`Commit files:`);
         await git(['add', '-A', '--ignore-errors'], onWorktreeOpts);
         await git(['commit', '-m', argv.message].concat(commitOpt), onWorktreeOpts);
       })
 
       // Add tag.
       .then(async () => {
-        if (argv.tag) {
-          await git(['tag', forceOpt, argv.tag], onWorktreeOpts);
-          isAddedTag = true;
-        }
+        if (argv.dryRun || !argv.tag) return;
+
+        debug(`Add tag: ${argv.tag}`);
+        await git(['tag', forceOpt, argv.tag], onWorktreeOpts);
+        isAddedTag = true;
       })
 
       // Push to remote repository.
       .then(async () => {
-        if (argv.remote) {
-          await git(['push', forceOpt, argv.remote, argv.branch], onWorktreeOpts);
-          if (argv.tag) {
-            await git(['push', forceOpt, argv.remote, argv.tag], onWorktreeOpts);
-          }
+        if (argv.dryRun || !argv.remote) return;
+
+        debug(`Push to remote repository: ${argv.remote} ${argv.branch}`);
+        await git(['push', forceOpt, argv.remote, argv.branch], onWorktreeOpts);
+        if (argv.tag) {
+          await git(['push', forceOpt, argv.remote, argv.tag], onWorktreeOpts);
         }
       })
 
       // Complete successflly.
-      .then(() => console.log(chalk.green(chalk.bold('completed successflly'))))
+      .then(() => console.log(chalk.green(chalk.bold(`completed successflly${argv.dryRun ? ' in dry-run mode' : ''}`))))
 
       // An exception was caught.
       .catch(err => {
