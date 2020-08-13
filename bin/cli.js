e673da880518a6661d4004b320497d6c8ad48e2c
Robin North <robin@allseeingeye.co>
2020-08-13 15:52:19 +0100

    fix: Typo

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 
 /* istanbul ignore if */
 if (process.version.match(/v(\d+)\./)[1] < 8) {
-  console.error('git-snapthot: Node v8 or greater is required. `git-snapthot` did not run.');
+  console.error('git-snapthot: Node v8 or greater is required. `git-snapshot` did not run.');
 } else {
   const gitSnapshot = require('..');
   const options = require('./options');
